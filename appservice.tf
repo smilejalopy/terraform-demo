63599f9ac43c9037064ce9eb13308b6f6f182d44
Wai Liu <waiholiu@users.noreply.github.com>
2024-08-29 19:26:41 +1000

    fadsfasdfasd

--- appservice.tf
+++ appservice.tf
@@ -13,7 +13,7 @@ resource "azurerm_app_service" "app" {
   name                = "${var.prefix}-wai-app"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  app_service_plan_id = azurerm_service_plan.example.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
