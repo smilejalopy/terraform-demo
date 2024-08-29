d25b0264cb727f9251c18a11a9a73fb01a10b51f
Wai Liu <waiholiu@users.noreply.github.com>
2024-08-29 18:07:56 +1000

    gsdfgfdsgdfs

--- providers.tf
+++ providers.tf
@@ -1,8 +1,11 @@
 terraform {
+
+  required_version = ">=1.2"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.74.0"
+      version = "~>3.7.0"
     }
   }
 
