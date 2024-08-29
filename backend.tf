cbd68f2d36f1d0d7665279b2efd3b8abaf4154a6
Wai Liu <waiholiu@users.noreply.github.com>
2024-08-29 16:22:20 +1000

    ARM

--- backend.tf
+++ backend.tf
@@ -8,10 +8,6 @@ terraform {
     storage_account_name = "terraformwsstatewai"
     container_name = "tfworkshop"
     key = "tfworkshop.tfstate"
-    use_oidc             = true # To use OIDC to authenticate to the backend
-    client_id            = var.client_id 
-    subscription_id      = var.subscription_id
-    tenant_id            = var.tenant_id
-    
+
   }
 }
