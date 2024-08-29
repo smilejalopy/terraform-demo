17e0f089f6d8d715c3d826cb4cbeda5a3fa583bb
aj-msft <ajajayakumar@microsoft.com>
2024-08-29 12:18:20 +1000

    Adding Landing Zone code

--- providers.tf
+++ providers.tf
@@ -2,10 +2,28 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=3.74.0"
     }
   }
 }
 provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+provider "azurerm" {
+  skip_provider_registration = true
+  alias           = "connectivity"
+  features {}
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  alias           = "management"
+  features {}
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  alias           = "identity"
   features {}
 }
