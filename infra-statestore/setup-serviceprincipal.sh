634c331e856f46eafafdc5f2716bc6dcae85f79a
Wai Liu <waiholiu@users.noreply.github.com>
2024-08-29 15:54:28 +1000

    stuff

--- infra-statestore/setup-serviceprincipal.sh
+++ infra-statestore/setup-serviceprincipal.sh
@@ -1,14 +1,18 @@
+# need to be done per branch
+
+
 $AZURE_TENANT = az account show -o tsv --query tenantId
 $SUBSCRIPTION_ID = az account show -o tsv --query id
 
-$APP_ID = az ad app create --display-name terraform-demo-oidc --query appId -otsv
+
+$APP_ID = az ad app create --display-name terraform-demo-action-oidc --query appId -otsv
 
 az ad sp create --id $APP_ID --query appId -otsv
 
 $OBJECT_ID = az ad app show --id $APP_ID --query id -otsv
 
 
-az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$OBJECT_ID/federatedIdentityCredentials" --body '{\"name\":\"terraform-demo-federated-identity-actions\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:waiholiu/terraform-demo:ref:refs/heads/actions\",\"description\":\"GitHub\",\"audiences\":[\"api://AzureADTokenExchange\"]}' --headers "Content-Type=application/json"
+az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$OBJECT_ID/federatedIdentityCredentials" --body '{\"name\":\"terraform-demo-actions-federated-identity\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:waiholiu/terraform-demo:ref:refs/heads/actions\",\"description\":\"GitHub\",\"audiences\":[\"api://AzureADTokenExchange\"]}' --headers "Content-Type=application/json"
 
 az role assignment create --assignee $APP_ID --role contributor --scope /subscriptions/$SUBSCRIPTION_ID
 az role assignment create --assignee $APP_ID --role 'User Access Administrator' --scope /subscriptions/$SUBSCRIPTION_ID
