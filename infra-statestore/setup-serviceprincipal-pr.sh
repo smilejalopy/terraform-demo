9604558ab3955710255c75867c15a4c7c4e31ee0
Wai Liu <waiholiu@users.noreply.github.com>
2024-08-30 10:53:31 +1000

    added pr client id

---
+++ infra-statestore/setup-serviceprincipal-pr.sh
@@ -0,0 +1,32 @@
+# need to be done per branch
+
+
+$AZURE_TENANT = az account show -o tsv --query tenantId
+$SUBSCRIPTION_ID = az account show -o tsv --query id
+
+
+# create a service principal
+$APP_ID = az ad app create --display-name terraform-demo-action-pr-oidc --query appId -otsv
+
+az ad sp create --id $APP_ID --query appId -otsv
+
+$OBJECT_ID = az ad app show --id $APP_ID --query id -otsv
+
+
+# create a federated identity to allow access 
+az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$OBJECT_ID/federatedIdentityCredentials" --body '{\"name\":\"terraform-demo-actions-pr-federated-identity\",\"issuer\":\"https://token.actions.githubusercontent.com\",\"subject\":\"repo:waiholiu/terraform-demo:pull_request\",\"description\":\"GitHub\",\"audiences\":[\"api://AzureADTokenExchange\"]}' --headers "Content-Type=application/json"
+
+
+# give the service principal access to the subscription
+az role assignment create --assignee $APP_ID --role contributor --scope /subscriptions/$SUBSCRIPTION_ID
+az role assignment create --assignee $APP_ID --role 'User Access Administrator' --scope /subscriptions/$SUBSCRIPTION_ID
+
+
+# to chuck into the github secrets
+
+# AZURE_SUBSCRIPTION_ID
+echo $SUBSCRIPTION_ID
+# AZURE_TENANT_ID
+echo $AZURE_TENANT
+# AZURE_CLIENT_ID
+echo $APP_ID
